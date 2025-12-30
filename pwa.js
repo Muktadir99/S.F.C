e15104569745211eac6448c57f8e8d264319eda1
Muktadir <109863553+Muktadir99@users.noreply.github.com>
2025-12-30 16:07:59 +0530

    Delete pwa.js

--- pwa.js
+++
@@ -1,7 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js", { scope: "/" })
-      .then(() => console.log("SW registered"))
-      .catch(err => console.log("SW failed", err));
-  });
-}
